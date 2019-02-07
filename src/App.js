cb1f5ee4d39d01cfedaca796e0ba2bb3485c8630
Wilian Revejes <wilianrevejes@hotmail.com>
2019-02-07 14:25:20 -0200

    JSXExpressions/ClassAndFunctionalComponents/Props/PropTypes/Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import Contact from './components/Contact';
+import Header from './components/Header';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>The App Component</h1>
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <Contact
+            name="John Doe"
+            email="jdoe@gmail.com"
+            phone="555-555-5555"
+          />
+          <Contact
+            name="Karen Smith"
+            email="karen@gmail.com"
+            phone="333-333-3333"
+          />
+        </div>
       </div>
     );
   }
