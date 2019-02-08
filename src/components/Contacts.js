273d1634a32fc647045d5837083ca1b622a1a774
Wilian Revejes <wilianrevejes@hotmail.com>
2019-02-08 14:40:22 -0200

    introState/Events/setState

---
+++ src/components/Contacts.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Contact from './Contact';
+class Contacts extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: 'John Doe',
+        email: 'jdoe@gmail.com',
+        phone: '555-555-5555'
+      },
+      {
+        id: 2,
+        name: 'karen Wiliams',
+        email: 'karen@gmail.com',
+        phone: '333-333-3333'
+      },
+      {
+        id: 3,
+        name: 'Henry Johnson',
+        email: 'henry@gmail.com',
+        phone: '111-111-1111'
+      }
+    ]
+  };
+
+  render() {
+    const { contacts } = this.state;
+
+    return (
+      <React.Fragment>
+        {contacts.map(contact => (
+          <Contact key={contact.id} contact={contact} />
+        ))}
+      </React.Fragment>
+    );
+  }
+}
+
+export default Contacts;
