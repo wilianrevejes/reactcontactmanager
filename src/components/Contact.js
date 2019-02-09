7d71d922918a3a2fe28262436b124e4365f97125
Wilian Revejes <wilianrevejes@hotmail.com>
2019-02-09 10:46:08 -0200

    bestPratices=>Constructor/Bind

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,24 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Contact extends Component {
-  state = {
-    showContactInfo: false
-  };
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      showContactInfo: false
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange() {
+    this.setState({ showContactInfo: !this.state.showContactInfo });
+  }
   render() {
     const { name, email, phone } = this.props.contact;
     const { showContactInfo } = this.state;
     return (
       <div className="card card-body mb-3 ">
         <h4>
-          {name}{' '}
-          <i
-            onClick={() =>
-              this.setState({ showContactInfo: !this.state.showContactInfo })
-            }
-            className="fas fa-sort-down"
-          />
+          {name} <i onClick={this.handleChange} className="fas fa-sort-down" />
         </h4>
         {showContactInfo ? (
           <ul className="list-group">
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import Contact from './Contact';
+import React, { Component } from "react";
+import Contact from "./Contact";
 class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: 'John Doe',
-        email: 'jdoe@gmail.com',
-        phone: '555-555-5555'
-      },
-      {
-        id: 2,
-        name: 'karen Wiliams',
-        email: 'karen@gmail.com',
-        phone: '333-333-3333'
-      },
-      {
-        id: 3,
-        name: 'Henry Johnson',
-        email: 'henry@gmail.com',
-        phone: '111-111-1111'
-      }
-    ]
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: [
+        {
+          id: 1,
+          name: "John Doe",
+          email: "jdoe@gmail.com",
+          phone: "555-555-5555"
+        },
+        {
+          id: 2,
+          name: "karen Wiliams",
+          email: "karen@gmail.com",
+          phone: "333-333-3333"
+        },
+        {
+          id: 3,
+          name: "Henry Johnson",
+          email: "henry@gmail.com",
+          phone: "111-111-1111"
+        }
+      ]
+    };
+  }
 
   render() {
     const { contacts } = this.state;
